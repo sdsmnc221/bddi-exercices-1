4a35b6098fd0aad2193bab0b5785535dac64670c
An Truong <antr.2201@gmail.com>
2019-09-17 10:36:12 +0200

    exo1

--- promises.js
+++ promises.js
@@ -2,25 +2,28 @@ function getUsers() {
   return new Promise((resolve, reject) => {
     // We simulate an async xhr call with setTimeout
     setTimeout(() => {
-      resolve(['Pierre', 'Paul', 'Jaques'])
-    }, Math.round(Math.random() * 5000))
-  })
+      resolve(['Pierre', 'Paul', 'Jaques']);
+    }, Math.round(Math.random() * 2000));
+  });
 }
 
 function createUser(name) {
   return new Promise((resolve, reject) => {
     // We simulate an async xhr call with setTimeout
     setTimeout(() => {
-      console.log(`${name} is created`)
-      resolve(name)
-    }, Math.round(Math.random() * 5000))
-  })
+      console.log(`${name} is created.`);
+      resolve(name);
+    }, Math.round(Math.random() * 2000));
+  });
 }
 
 function createUsers(users) {
-  return users.map((user) => {
-    return createUser(user)
-  })
+  return users.map(user => {
+    return createUser(user);
+  });
 }
 
 // Votre code ici (8 lignes)
+getUsers()
+  .then(users => Promise.all(createUsers(users)))
+  .then(() => console.log('All users are created.'));
